271cbbc056dfad832a523d7dc8f528e0ca480ad7
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-02-07 19:45:44 -0500

    program created

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,3 +1,15 @@
+/**
+ * @Class: Cylinder
+ * @author: Olufunmilayo Ekpereh
+ * version 1.0
+ * Course: ITEC 2140 Spring 2023
+ * Written: February 7, 2023
+ * This program will calculate the base area and volume of a cylinder, given the radius of
+ * the base circle and length of the cylinder.
+ * In this program, I will declare variables: radius, length, baseArea, and volume.
+ */
+
+
 import java.util.Scanner;
 public class Cylinder {
     public static void main(String[] args){
@@ -11,5 +23,6 @@ public class Cylinder {
         double volume = baseArea * length; //to get volume of a cylinder
         System.out.println("The base area is: " +baseArea);
         System.out.println("The volume is: " +volume);
+
     }
 }
